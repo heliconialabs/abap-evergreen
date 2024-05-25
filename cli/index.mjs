4dafb17c00f416c6fb8150e2bf63395fd3aa1e9c
Lars Hvam <larshp@hotmail.com>
2024-05-25 14:22:24 +0200

    something like this

--- cli/index.mjs
+++ cli/index.mjs
@@ -1,9 +1,37 @@
 import * as fs from "node:fs";
+import * as path from "node:path";
 import * as glob from "glob";
 
 async function run() {
   const files = glob.sync("src/**/*.*", {nosort: true, nodir: true});
-  console.dir(files);
+
+  for (const filename of files) {
+    if (filename.endsWith(".xml") === false
+        || filename.endsWith("package.devc.xml")) {
+      continue;
+    }
+
+    console.log(filename);
+    const input = fs.readFileSync(filename, "utf8");
+
+    const result = await fetch("http://localhost:8080/evergreen", {
+      method: "POST",
+      headers: {
+        "Content-Type": "text/xml",
+        "x-filename": path.basename(filename),
+      },
+      body: input,
+    });
+
+    if (result.ok === false) {
+      throw new Error(`Failed to fetch: ${result.statusText}`);
+    }
+
+    const output = await result.text();
+    if (output !== input) {
+      fs.writeFileSync(filename, output);
+    }
+  }
 }
 
 await run();
